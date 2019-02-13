804641391ab87f69552187a57787a9f3eeabdd2a
LuisGalvan <galvan7725@gmail.com>
2019-02-12 19:50:38 -0600

    Continuar en el login y agregar el navbar

---
+++ src/java/Beans/loginBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author galva
+ */
+@Named(value="loginBean")
+@RequestScoped
+public class loginBean {
+    
+}
